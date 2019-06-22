353f640450c025925d11ba12a825b1ae388758b7
Alexandre Iooss <erdnaxe@crans.org>
2019-06-22 07:46:35 +0200

    Revert "Remove Django contenttype"
    This reverts commit 1766ca7f3dbbd78d62679f5b1c9249e838293559.

--- portail_captif/settings.py
+++ portail_captif/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
